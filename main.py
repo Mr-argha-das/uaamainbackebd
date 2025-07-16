0ca15001711a08bb73415910e3431bdfe54635e6
abhaykoli1 <abhaykoli214@gmail.com>
2025-07-16 10:14:11 +0530

    changes

--- main.py
+++ main.py
@@ -2,13 +2,13 @@
 from mongoengine import connect
 from fastapi import FastAPI
 import uvicorn
+
 from AvBigBuddy.AvCounters.routes import AvCounterRoutes
 from AvBigBuddy.AvServices.routes import AvServiceRoute
 from AvBigBuddy.AvProducts.routes import AvProductsRoute
 from AvBigBuddy.AvMembers.routes import AvMemberRoute
 from AvBigBuddy.AvQueries.routes import avcontactroutes
 from homeImages.routes import homeImagesRoutes
-
 from blogs.routes import blogroutes
 from homePageQuery.routes import homePageRoutes
 from query.routes import contactroutes
@@ -40,12 +40,9 @@ app.include_router(AvServiceRoute.router, tags=['Av Services'])
 app.include_router(AvProductsRoute.router, tags=['Av Products'])
 app.include_router(AvMemberRoute.router, tags=['Av Members'])
 app.include_router(avcontactroutes.router, tags=["Av contact query"])
-
-
 app.include_router(homeImagesRoutes.router, tags=["Home Images"])
 app.include_router(serviceroutes.router, tags=['service'])
 app.include_router(counterRoutes.router, tags=['counters'])
-
 app.include_router(blogroutes.router, tags=["Blog routes"])
 app.include_router(sampleroutes.router, tags=["Sample"])
 app.include_router(sampleCategoryRoutes.router, tags=["Sample Category"])
